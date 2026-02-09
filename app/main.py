28be075f3454403d4e3f2c0777d61392c2bf286f
divijgera <divijgera97@gmail.com>
2026-02-09 01:24:13 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,10 +5,18 @@ def main():
     with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
         conn, addr = server_socket.accept()  # wait for client
         print(f"Connection from {addr}")
+        
         request = conn.recv(4096).decode("utf-8")
         print(f"Received request: {request}")
-        print("Sending response...")
-        response = "HTTP/1.1 200 OK\r\n\r\n"
+
+        path = request.split("\r\n")[0].split(" ")[1]
+        print(f"Requested path: {path}")
+
+        if path == "/":
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
         conn.sendall(response.encode("utf-8"))
         conn.close()
 
