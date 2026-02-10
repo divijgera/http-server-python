5d7e1cb33310643430ab73c639784effca1b8249
divijgera <divijgera97@gmail.com>
2026-02-10 00:54:05 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,26 @@ def handle_echo(path: str) -> str:
     message = path[len("/echo/"):]
     return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(message)}\r\n\r\n{message}"
 
+def extract_request_info_from_request(request: str) -> tuple[str, str, str]:
+    request_info = request.split("\r\n")[0].split(" ")
+    method = request_info[0]
+    path = request_info[1]
+    protocol = request_info[2]
+    return method, path, protocol
+
+def extract_headers_from_request(request: str) -> dict[str, str]:
+    headers = {}
+    for line in request.split("\r\n")[1:]:
+        if line == "":
+            # An empty line indicates the end of the headers
+            break
+        key, value = line.split(": ", 1)
+        headers[key] = value
+    return headers
+
+def extract_body_from_request(request: str) -> str:
+    return request.split("\r\n\r\n", 1)[1] if "\r\n\r\n" in request else ""
+
 def main():
     with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
         conn, addr = server_socket.accept()  # wait for client
@@ -12,11 +32,26 @@ def main():
         request = conn.recv(4096).decode("utf-8")
         print(f"Received request: {request}")
 
-        path = request.split("\r\n")[0].split(" ")[1]
-        print(f"Requested path: {path}")
+        method, path, protocol = extract_request_info_from_request(request)
+        headers = extract_headers_from_request(request)
+        body = extract_body_from_request(request)
+        print(
+            "\n".join(
+                [
+                    f"Requested method: {method}",
+                    f"Requested path: {path}",
+                    f"Requested protocol: {protocol}",
+                    f"Requested headers: {headers}",
+                    f"Requested body: {body}",
+                ]
+            )
+        )
 
         if path.startswith("/echo/"):
             response = handle_echo(path)
+        elif path == "/user-agent":
+            user_agent_header = headers.get("User-Agent", "Unknown")
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent_header)}\r\n\r\n{user_agent_header}"
         elif path == "/":
             response = "HTTP/1.1 200 OK\r\n\r\n"
         else:
